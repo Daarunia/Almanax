fef695f860878ef8b024c2ca9d487923807fde1e
Daarunia <maximevaneygen@gmail.com>
2026-07-13 21:11:57 +0200

    feat: add french translations for date picker

--- src/main.ts
+++ src/main.ts
@@ -12,5 +12,22 @@ app.use(PrimeVue, {
       darkModeSelector: ".my-app-dark",
     },
   },
+  locale: {
+    firstDayOfWeek: 1,
+    dayNames: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"],
+    dayNamesShort: ["dim", "lun", "mar", "mer", "jeu", "ven", "sam"],
+    dayNamesMin: ["D", "L", "M", "M", "J", "V", "S"],
+    monthNames: [
+      "janvier", "février", "mars", "avril", "mai", "juin",
+      "juillet", "août", "septembre", "octobre", "novembre", "décembre",
+    ],
+    monthNamesShort: [
+      "janv", "févr", "mars", "avr", "mai", "juin",
+      "juil", "août", "sept", "oct", "nov", "déc",
+    ],
+    today: "Aujourd'hui",
+    clear: "Effacer",
+    dateFormat: "dd/mm/yy",
+  },
 });
 app.mount("#app");
